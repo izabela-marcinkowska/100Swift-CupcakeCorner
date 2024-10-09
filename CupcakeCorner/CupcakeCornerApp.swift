1f035cece36ebf9f3cf95e763ebab5233ce46992
Devy <izabela.marcinkowska@outlook.com>
2024-10-09 19:47:01 +0200

    first commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Izabela Marcinkowska on 2024-10-09.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
