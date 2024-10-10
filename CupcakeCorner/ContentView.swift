d19b0eec76d06591ef9ffbdd50b4cc048c9a822a
Devy <izabela.marcinkowska@outlook.com>
2024-10-10 19:46:03 +0200

    Showing picture from API example

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -19,35 +19,21 @@ struct Result: Codable {
 
 
 struct ContentView: View {
-    @State private var results = [Result]()
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
-    }
+   
     
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
             }
-           
-            } .task {
-                await loadData()
         }
+        .frame(width: 200, height: 200)
     }
 }
 
