5f4ce5ba507320b80916dc4401cdc55c722f00f6
Devy <izabela.marcinkowska@outlook.com>
2024-10-14 15:28:37 +0200

    feat: Add form for cake

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,18 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Izabela Marcinkowska on 2024-10-14.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView()
+}
--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -19,21 +19,29 @@ struct Result: Codable {
 
 
 struct ContentView: View {
-   
-    
+    @State private var order = Order()
     var body: some View {
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil {
-                Text("There was an error loading the image.")
-            } else {
-                ProgressView()
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
             }
+            .navigationTitle("Cupcake Corner")
         }
-        .frame(width: 200, height: 200)
     }
 }
 
