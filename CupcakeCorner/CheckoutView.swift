8fac48d4eada7e8ead9e72d2a023b24406faec50
Devy <izabela.marcinkowska@outlook.com>
2024-10-14 17:00:33 +0200

    feat: Validation on the adress view

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Izabela Marcinkowska on 2024-10-14.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
